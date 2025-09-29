ab0c514dfa9b719669e8b93012b7871185e03000
LavyaVats <cu25870001@coeruniversity.ac.in>
2025-09-29 09:12:56 +0530

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -1,1 +1,12 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+    for (int i = 1; i <= 10; i++) {
+        if (i == 5) {
+            continue; 
+        }
+        cout << i << " ";
+    }
+    return 0;
+}
